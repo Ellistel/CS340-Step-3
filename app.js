66c1ce86fb1a0d3c4f81f3d5026b3828a3b4b974
Luke Ellister <ellistel@flip4.engr.oregonstate.edu>
2024-02-28 13:18:20 -0800

    serving customers page with handlebars/ database

--- app.js
+++ app.js
@@ -2,34 +2,36 @@
     SETUP
 */
 
-// Express
 var express = require('express');
-var app = express();
-PORT = 5321;
+var hbs = require('hbs');
+var path = require('path');
 
+var app = express();
+app.set('view engine', 'html')
+
+app.set('views', path.join(__dirname, 'static'));
+app.engine('html', require('hbs').__express);
+
+PORT = 2424;
 // Database
-var db = require('./database/db-connector')
+var db = require('./database/db-connector');
 
 /*
     ROUTES
 */
+//Static
+app.use('/',express.static(path.join(__dirname, 'static')))
 
-app.get('/', function(req,res)
-{
-    let query1 = 'SELECT * FROM Customers'
-    db.pool.query(query1, function(error, rows, fields)
-    {
-        res.render('customers', {data:rows})
-
-    })
-}
-
-
-)
+app.get('/Customers', function(req, res) {
+    let query1 = 'SELECT * FROM Customers';
+    db.pool.query(query1, function(error, rows, fields) {
+        res.render('customers', { data: rows });
+    });
+});
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+app.listen(PORT, function() {
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
 });
