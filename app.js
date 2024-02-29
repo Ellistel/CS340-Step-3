838380af5c4c77c5f955186e84d6f18a4b7f6670
Luke Ellister <ellistel@flip4.engr.oregonstate.edu>
2024-02-28 19:01:00 -0800

    got basic insert and delete functionalities working for the Customer Page

--- app.js
+++ app.js
@@ -5,30 +5,81 @@
 var express = require('express');
 var hbs = require('hbs');
 var path = require('path');
-
+var express = require('express')
+var exphbs = require("express-handlebars")
+const bodyParser = require('body-parser');
+const e = require('express')
 var app = express();
-app.set('view engine', 'html')
+app.engine("handlebars", exphbs.engine({ defaultLayout: false }))
+app.set("view engine", "handlebars")
 
-app.set('views', path.join(__dirname, 'static'));
-app.engine('html', require('hbs').__express);
+PORT = 2425;
 
-PORT = 2424;
 // Database
 var db = require('./database/db-connector');
 
+let query2 = `INSERT INTO Customers(customerName, customerContact) VALUES (?, ?)`;
+let customerDelete = `DELETE FROM Customers WHERE customerID = ?;`
 /*
     ROUTES
 */
 //Static
-app.use('/',express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/Customers', function(req, res) {
     let query1 = 'SELECT * FROM Customers';
     db.pool.query(query1, function(error, rows, fields) {
-        res.render('customers', { data: rows });
+
+        const formattedRows = rows.map(customer => ({
+            ID: customer.customerID,
+            'Customer Name': customer.customerName,
+            'Customer Contact': customer.customerContact,
+            Actions:''
+        }));
+        // Now, pass the modified 'formattedRows' data to the rendering engine
+        res.render('customerview', { title: formattedRows, data: rows });
+
+
+
     });
 });
 
+
+    app.post('/add-customer-form', function(req, res) {
+    console.log("I'M IN HERE");
+    let data = req.body;
+
+    let name = data['cname'];
+    let contact = data['contact'];
+    
+    db.pool.query(query2, [name, contact], function(error, rows, fields) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            res.redirect('/Customers');
+        }
+    });
+});
+
+app.post('/deleteCustomer/:customerID', function(req, res)
+    {
+        let customerId = req.params.customerID;
+        console.log("Im in here")
+        console.log(req.body)
+        db.pool.query(customerDelete, customerId, function (err, results, fields){
+
+                        // Send the results to the browser
+                        if(err){
+                            console.log(err)
+                            res.sendStatus(500)
+                        }
+                        else
+                        res.redirect("/Customers")
+
+        });
+    });
+    app.use('/',express.static(path.join(__dirname, 'static')))
 /*
     LISTENER
 */
